d91f682cf38ba96aa89df64a44ea00b87c3f7543
Jenni K <jennik3379@gmail.com>
2015-12-24 21:26:19 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+##I Put together a basic Matrix that I set uniquely. 
 
-## Write a short comment describing this function
+## This function creates a unique matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
+My solution
+makeCacheMatrix <-function(x=matrix()){
+   inv <-NULL
+   set<-function(y) {
+     x<<-y
+     inv<<-NULL
+ }
+ get <-function() x
+ SetInverse<-function(inverse)inv<<-inverse
+ getInverse<-function()inv
+ list(set=set,
+      get=get,
+      setInverse=setInverse,
+      getInverse=getInverse)
+ }     
 
-
-## Write a short comment describing this function
+##this should mke the inverse on the unique matrix made above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   inv <-x$getInverse()
+   if (!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+}          
+mat <-Xget$()
+inv<- solve(mat, ...)
+x$setInverse(inv)
+inv
 }
+> source('~/Rprogassign2/Rprogassign2/cachematrix.R')
+> my_matrix<-makeCacheMatrix(matrix(1:4, 2, 2))
+> my_matrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> my_matrix$getInverse()
+NULL
+> cacheSolve(my_matrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> setwd("~/Rprogassign2/Rprogassign2")
+> setwd("~/Rprogassign2/Rprogassign2")
